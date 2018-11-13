25dd1b1bbc9312bdc641781f3c70d7bf953021f0
Michiocre <michi2806@gmail.com>
2018-11-13 23:28:05 +0100

    Revert

--- spoti-vote-backend/server.js
+++ spoti-vote-backend/server.js
@@ -19,22 +19,14 @@ const io = socketIo(server);
 
 //Global Varibles
 
-const ipAddress = process.env.ADDRESS || 'localhost';       //Wichtig env.ADDRESS = 'spoti-vote.com' -> wen local egal
-const port = process.env.PORT || 80;                        //Wichtig env.PORT = 443 -> wenn local egal
-const backExtension = process.env.BACKEXT || '';      //wichtig env.BACKEXTENSION = '/b' -> Wenn local egal
-const portBack = 8888;
-
-const uriFront = (backExtension == '' ? 'http://' + ipAddress + ':' + port : 'https://' + ipAddress + ':' + port)
-const uriBack = (backExtension == '' ? 'http://' + ipAddress + ':' + portBack : 'https://' + ipAddress + ':' + port + backExtension);
-
-const redirect_uri = uriBack + '/callback';
-
-
+const ipAddress = process.env.ADDRESS || 'localhost';
+const portFront = process.env.PORT || 80;
+const portBack = process.env.PORTBACK || 8888;
+const redirect_uri = 'http://' + ipAddress + ':' + portBack + '/callback';
 
 
 const secTillDelete = 60;
 console.log('INFO: Redirect URL: ' + redirect_uri);
-console.log('INFO: backExtension: ' + backExtension + ' yeet ' + process.env.BACKEXT);
 
 let rooms = [];
 let users = [];
@@ -123,7 +115,7 @@ app.get(backExtension + '/callback', async (req, res) => {
 		json: true
 	};
 	request.post(authOptions, async (error, response, body) => {
-		let uri = uriFront + '/dashboard';
+		let uri = 'http://' + ipAddress + ':' + portFront + '/dashboard';
         let user = new User(body.access_token, body.refresh_token, process.env.SPOTIFY_CLIENT_ID, process.env.SPOTIFY_CLIENT_SECRET);
 
         // Set cookie
@@ -145,7 +137,7 @@ app.get(backExtension + '/callback', async (req, res) => {
 */
 app.get(backExtension + '/createRoom', async (req, res) => {
     let room = new Room(users[0], rooms);
-    let uri = uriFront + '/app';
+    let uri = 'http://' + ipAddress + ':' + portFront + '/app';
 
     console.log(room);
 
